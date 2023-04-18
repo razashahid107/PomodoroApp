53f7a269d8a5a4b7c1d587c4e04dc20c6ab48d69
razashahid107 <razashahid107@gmail.com>
2023-04-18 05:16:37 +0500

    Basic structure with MainAcitivity.xml and MainActivity.java updated

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.CountDownTimer;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    public int counter;
+    TextView textview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.StartPause);
+        textview = (TextView)findViewById(R.id.countdown_timer);
+        // Timer has been created in a text view
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(30000, 1000){
+                    public void onTick(long millisUntilFinished){
+                        textview.setText(String.valueOf(counter));
+                        counter++;
+                    }
+                    public void onFinish(){
+                        textview.setText("Finish");
+                    }
+                };
+            }
+        });
+    }
+    public void StartPause(View v){
+
+    }
+}
