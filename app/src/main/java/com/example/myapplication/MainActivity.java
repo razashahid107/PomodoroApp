47d2d84bab3248cece3426325a0d907cdaffddd1
razashahid107 <razashahid107@gmail.com>
2023-04-21 22:28:48 +0500

    begin new branch

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,35 +6,38 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.CountDownTimer;
-
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     public int counter;
     TextView textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.StartPause);
-        textview = (TextView)findViewById(R.id.countdown_timer);
+        textview = (TextView) findViewById(R.id.countdown_timer);
         // Timer has been created in a text view
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new CountDownTimer(30000, 1000){
-                    public void onTick(long millisUntilFinished){
+                new CountDownTimer(30000, 1000) {
+                    public void onTick(long millisUntilFinished) {
                         textview.setText(String.valueOf(counter));
                         counter++;
                     }
-                    public void onFinish(){
+
+                    public void onFinish() {
                         textview.setText("Finish");
                     }
                 };
             }
         });
     }
-    public void StartPause(View v){
 
+    public void StartPause(View v) {
+        Log.d("success", "startwasclicked");
     }
 }
