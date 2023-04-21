65968242d842f2e044a6a1580144cda0a6fa8579
razashahid107 <razashahid107@gmail.com>
2023-04-22 00:55:34 +0500

    Added Toast when Start is clicked

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,12 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.CountDownTimer;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -18,26 +23,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.StartPause);
-        textview = (TextView) findViewById(R.id.countdown_timer);
-        // Timer has been created in a text view
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new CountDownTimer(30000, 1000) {
-                    public void onTick(long millisUntilFinished) {
-                        textview.setText(String.valueOf(counter));
-                        counter++;
-                    }
-
-                    public void onFinish() {
-                        textview.setText("Finish");
-                    }
-                };
+                Toast.makeText(MainActivity.this, "This button was clicked", Toast.LENGTH_SHORT).show();
             }
         });
+//        textview = (TextView) findViewById(R.id.countdown_timer);
+//        // Timer has been created in a text view
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new CountDownTimer(30000, 1000) {
+//                    public void onTick(long millisUntilFinished) {
+//                        textview.setText(String.valueOf(counter));
+//                        counter++;
+//                    }
+//
+//                    public void onFinish() {
+//                        textview.setText("Finish");
+//                    }
+//                };
+//            }
+//        });
     }
 
     public void StartPause(View v) {
-        Log.d("success", "startwasclicked");
+//        Log.d("success", "startwasclicked");
+//        AlertDialog alertDialog = alertDialogBuilder.create();
+//        alertDialog.show();
+//        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("my message is thiss");
+        builder1.setCancelable(true);
+        builder1.show();
     }
 }
