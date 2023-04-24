c57cd82c612147161cd2127e3a37e1810fa1fc24
razashahid107 <razashahid107@gmail.com>
2023-04-24 20:52:02 +0500

    Upon clicking of start, the text field gets updated to text saying this button was cliked

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.os.CountDownTimer;
 import android.app.AlertDialog;
@@ -13,22 +14,33 @@ import android.content.DialogInterface;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     Button button;
     public int counter;
     TextView textview;
+    EditText input_field;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // this one is responsible to create the activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        textview = (TextView) findViewById(R.id.myTextView);
+//        textview = (TextView) findViewById(R.id.editTextTextPersonName2);
+        textview = (TextView) findViewById(R.id.myTextView);
+        input_field = (EditText) findViewById(R.id.inputField);
         button = (Button) findViewById(R.id.StartPause);
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() { // this is running through the button variable we've set above
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "This button was clicked", Toast.LENGTH_SHORT).show();
+                textview.setText("the start button was clicked and this worked");
             }
         });
+
+//        textview.setText();
+
 //        textview = (TextView) findViewById(R.id.countdown_timer);
 //        // Timer has been created in a text view
 //        button.setOnClickListener(new View.OnClickListener() {
@@ -47,17 +59,16 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
-
-    public void StartPause(View v) {
-//        Log.d("success", "startwasclicked");
-//        AlertDialog alertDialog = alertDialogBuilder.create();
-//        alertDialog.show();
-//        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setMessage("my message is thiss");
-        builder1.setCancelable(true);
-        builder1.show();
-    }
+//    public void StartPause(View v) {
+////        Log.d("success", "startwasclicked");
+////        AlertDialog alertDialog = alertDialogBuilder.create();
+////        alertDialog.show();
+////        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+////        AlertDialog dialog = builder.create();
+////        dialog.show();
+//        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+//        builder1.setMessage("my message is thiss");
+//        builder1.setCancelable(true);
+//        builder1.show();
+//    }
 }
