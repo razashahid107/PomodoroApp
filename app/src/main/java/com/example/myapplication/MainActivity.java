b931721a859c349755b0f57ea46c3ae3abbdb397
razashahid107 <razashahid107@gmail.com>
2023-05-08 17:30:16 +0500

    only one timer can run at a time

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.os.CountDownTimer;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textview;
     EditText input_space;
     EditText input_space_min;
-//    public CharSequence user_time_input;
+    boolean timer_check = false;
+
+    //    public CharSequence user_time_input;
 //    public int user_time_input_int;
     //    CharSequence x;
 //    int number;
@@ -44,17 +46,26 @@ public class MainActivity extends AppCompatActivity {
                 CharSequence user_time_input_min = input_space_min.getText();
                 int user_time_input_int = Integer.parseInt(user_time_input.toString());
                 int user_time_input_int_min = Integer.parseInt(user_time_input_min.toString());
-//                Log.d(null, "this is my number" + number);
-                new CountDownTimer((user_time_input_int_min * 60*1000)+user_time_input_int*1000, 1000) {
-                    public void onTick(long millisUntilFinished) {
+
+                if (timer_check){
+                    Toast.makeText(getApplicationContext(),"A timer is alredy in place. Stop it to start a new one.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    new CountDownTimer((user_time_input_int_min * 60*1000)+user_time_input_int*1000, 1000) {
+                        public void onTick(long millisUntilFinished) {
+                            timer_check = true;
 //                        textview.setText(String.valueOf(counter));
-                        textview.setText("Time Left: " + millisUntilFinished/1000);
-                        counter++;
-                    }
-                    public void onFinish() {
-                        textview.setText("Finish");
-                    }
-                }.start();
+                            textview.setText("Time Left: " + millisUntilFinished/1000);
+                            counter++;
+                        }
+                        public void onFinish() {
+                            textview.setText("Finish");
+                            timer_check = false;
+                        }
+                    }.start();
+                }
+
+
 
             }
         });
