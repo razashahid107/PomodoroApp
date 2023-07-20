4dd73c46bc90827d8a6da7484265fbbd10b31d33
razashahid107 <razashahid107@gmail.com>
2023-07-20 21:01:12 +0500

    idea: inclued counter to possibly save seconds value and use it later if I'd edit up the start/stop functionlaity of the function

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -36,35 +36,42 @@ public class MainActivity extends AppCompatActivity {
         textview_sample = (TextView)findViewById(R.id.sample_space);
         textview.setText("Touch start to start timer :)");
         textview.setInputType(InputType.TYPE_CLASS_NUMBER);
-//        String z = null;
 
         // Timer has been created in a text view
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // change text of button to stop
-                button.setText("Stop");
 
-                CharSequence user_time_input = input_space.getText(); // we get text from here
-                CharSequence user_time_input_min = input_space_min.getText();
-                int user_time_input_int = Integer.parseInt(user_time_input.toString());
-                int user_time_input_int_min = Integer.parseInt(user_time_input_min.toString());
-
+//                CharSequence user_time_input = input_space.getText(); // we get text from here
+//                CharSequence user_time_input_min = input_space_min.getText();
+                //Access user inputs
+                int user_time_input_int = Integer.parseInt(input_space.getText().toString());
+                int user_time_input_int_min = Integer.parseInt(input_space_min.getText().toString());
+                Log.d("CLICK()", "you clicked the button again ");
+                counter = (user_time_input_int_min * 60*1000) + (user_time_input_int*1000);
                 if (timer_check){
-                    Toast.makeText(getApplicationContext(),"A timer is alredy in place. Stop it to start a new one.", Toast.LENGTH_SHORT).show();
+                    timer_check = false;
+                    button.setText("START"); // change text of button to stop
+//                    Toast.makeText(getApplicationContext(),"A timer is alredy in place. Stop it to start a new one.", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    new CountDownTimer((user_time_input_int_min * 60*1000)+user_time_input_int*1000, 1000) {
+                    button.setText("STOP");
+                    timer_check = true;
+                    Log.d("GETOUT", "The function is exiting and relooping");
+                    new CountDownTimer(counter, 1000) {
                         public void onTick(long millisUntilFinished) {
-                            timer_check = true;
+//                            onFinish();
 //                        textview.setText(String.valueOf(counter));
+                            Log.d("onTick()", "Seconds left are "+ millisUntilFinished);
                             textview.setText("Time Left: " + millisUntilFinished/1000);
 //                            textview_sample.setText("hellooo");
-                            counter++;
+                            counter-=1000;
+                            Log.d("COUNTER", "counter out: "+counter);
+                            onFinish();
                         }
                         public void onFinish() {
-                            textview.setText("Finish");
-                            timer_check = false;
+//                            textview.setText("Finish");
+//                            timer_check = false;
                         }
                     }.start();
                 }
