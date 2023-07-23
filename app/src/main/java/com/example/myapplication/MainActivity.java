e2d951f745f4b65e7f9b2705f7aa388611a15928
razashahid107 <razashahid107@gmail.com>
2023-07-23 17:02:56 +0500

    added the stop functionality. timer still running 2 timers simultaneously:

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,13 +14,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    public int counter;
+    public int counter=0;
 //    public long number;
     TextView textview;
     EditText input_space;
     TextView textview_sample;
     EditText input_space_min;
-    boolean timer_check = false;
+    boolean timer_check = true;
     //    public CharSequence user_time_input;x 
 //    public int user_time_input_int;
     //    CharSequence x;
@@ -41,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (counter>0 && timer_check==true) {
+                    timer_check=false;
+                } else if (counter>0 && timer_check==false) {
+                    timer_check=true;
+                }
 
 //                CharSequence user_time_input = input_space.getText(); // we get text from here
 //                CharSequence user_time_input_min = input_space_min.getText();
@@ -49,25 +54,40 @@ public class MainActivity extends AppCompatActivity {
                 int user_time_input_int_min = Integer.parseInt(input_space_min.getText().toString());
                 Log.d("CLICK()", "you clicked the button again ");
                 counter = (user_time_input_int_min * 60*1000) + (user_time_input_int*1000);
-                if (timer_check){
-                    timer_check = false;
-                    button.setText("START"); // change text of button to stop
+//                if (timer_check){
+//                    timer_check = false;
+//                    button.setText("START"); // change text of button to Start
 //                    Toast.makeText(getApplicationContext(),"A timer is alredy in place. Stop it to start a new one.", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    button.setText("STOP");
-                    timer_check = true;
+//                }
+//                else {
+//                    button.setText("STOP");
+//                    timer_check = true;
                     Log.d("GETOUT", "The function is exiting and relooping");
                     new CountDownTimer(counter, 1000) {
                         public void onTick(long millisUntilFinished) {
 //                            onFinish();
 //                        textview.setText(String.valueOf(counter));
-                            Log.d("onTick()", "Seconds left are "+ millisUntilFinished);
-                            textview.setText("Time Left: " + millisUntilFinished/1000);
+//                            Log.d("onTick()", "Seconds left are "+ millisUntilFinished);
+//                            textview.setText("Time Left: " + millisUntilFinished/1000);
 //                            textview_sample.setText("hellooo");
-                            counter-=1000;
-                            Log.d("COUNTER", "counter out: "+counter);
-                            onFinish();
+
+                            // i can try to put all the if else loop inside here
+                            if(timer_check){
+                                button.setText("STOP");
+                                Log.d("onTick()", "Seconds left are "+ millisUntilFinished);
+                                textview.setText("Time Left: " + millisUntilFinished/1000);
+                                counter-=1000;
+                                Log.d("COUNTER", "counter out: "+counter);
+
+                            }
+                            else { // if timer_check=false run onFinish()
+//                                timer_check = false;
+                                button.setText("START"); // change text of button to Start
+                                onFinish();
+                            }
+
+
+//                            onFinish();
                         }
                         public void onFinish() {
 //                            textview.setText("Finish");
@@ -75,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }.start();
                 }
-            }
         });
     }
 }
