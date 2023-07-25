6c3711344ac141e8af449265c7704b5184c5a8aa
razashahid107 <razashahid107@gmail.com>
2023-07-25 19:50:13 +0500

    start v

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.os.CountDownTimer;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -19,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
     public int counter = 0; // used to record time
     TextView textview;
-    EditText input_space;
-    TextView textview_sample;
+    EditText input_space_sec;
     EditText input_space_min;
+    TextView textview_sample;
+
     boolean timer_check = true; // determines whether timer is running or not. True by default so timer can run
     boolean break_check = false;
 
@@ -72,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input_space = (EditText) findViewById(R.id.user_input);
+        input_space_sec = (EditText) findViewById(R.id.user_input_second);
         input_space_min = (EditText) findViewById(R.id.user_input_min);
         button = (Button) findViewById(R.id.StartPause);
         textview = (TextView) findViewById(R.id.countdown_timer); // Timer exists in textview
@@ -82,47 +81,53 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // break checks
-                if (break_check){ // this means the break is going on already and user wants it to end
-                    break_check = false;
-                    startBreakTimer();
-                }
-                // following condition exists for the second time when button is clicked
-                if (counter > 0 && timer_check == true) {
-                    Log.d("COUNTER", "counter is:" + counter);
-                    timer_check = false;
-                } else if (counter > 0 && timer_check == false) {
-                    timer_check = true;
-                }
+//                // break checks
+//                if (break_check){ // this means the break is going on already and user wants it to end
+//                    break_check = false;
+//                    startBreakTimer();
+//                }
+//                // following condition exists for the second time when button is clicked
+//                if (counter > 0 && timer_check == true) {
+//                    Log.d("COUNTER", "counter is:" + counter);
+//                    timer_check = false;
+//                } else if (counter > 0 && timer_check == false) {
+//                    timer_check = true;
+//                }
+//
+//                // Access user inputs for timer duration
+//                int user_time_input_int = Integer.parseInt(input_space.getText().toString());
+//                int user_time_input_int_min = Integer.parseInt(input_space_min.getText().toString());
+//
+//                // Use inputs from user to input time
+//                counter = (user_time_input_int_min * 60 * 1000) + (user_time_input_int * 1000);
+//
+//                timer = new CountDownTimer(counter, 1000) {
+//                    public void onTick(long millisUntilFinished) {
+//                        if (timer_check) {
+//                            button.setText("END");
+//                            textview.setText("Time Left: " + millisUntilFinished / 1000);
+//                            counter -= 1000;
+////                            Break();
+//                        }
+//                        else { // if timer_check=false run cancel()
+//                            button.setText("START AGAIN"); // change text of button to Start
+//                            textview.setText("Timer Ended");
+//                            cancel();
+//                        }
+//                    }
+//                    public void onFinish() {
+////                        textview.setText("Timer Ended");
+//                        break_check=true;
+//                        startBreakTimer(); // Start the break timer automatically after the main timer ends
+//
+//                    }
+//                }.start();
 
-                // Access user inputs for timer duration
-                int user_time_input_int = Integer.parseInt(input_space.getText().toString());
+                // Access user input
+                int user_time_input_int = Integer.parseInt(input_space_sec.getText().toString());
                 int user_time_input_int_min = Integer.parseInt(input_space_min.getText().toString());
-                
-                // Use inputs from user to input time
-                counter = (user_time_input_int_min * 60 * 1000) + (user_time_input_int * 1000); 
 
-                timer = new CountDownTimer(counter, 1000) {
-                    public void onTick(long millisUntilFinished) {
-                        if (timer_check) {
-                            button.setText("END");
-                            textview.setText("Time Left: " + millisUntilFinished / 1000);
-                            counter -= 1000;
-//                            Break();
-                        } 
-                        else { // if timer_check=false run cancel()
-                            button.setText("START AGAIN"); // change text of button to Start
-                            textview.setText("Timer Ended");
-                            cancel();
-                        }
-                    }
-                    public void onFinish() {
-//                        textview.setText("Timer Ended");
-                        break_check=true;
-                        startBreakTimer(); // Start the break timer automatically after the main timer ends
 
-                    }
-                }.start();
             }
         });
     }
